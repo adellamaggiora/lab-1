3cc5954f74886cdb6995ef737925450774051ebb
Fabrizio <52281394+Royalsynth@users.noreply.github.com>
2024-07-22 12:11:26 +0200

    Delete rotr.js

--- exercises/todo/rotr.js
+++
@@ -1,4 +0,0 @@
-String.prototype.rotr = function() {
-    if (this.length === 0) return this.toString() // Restituisce la stringa immutata se è vuota
-    return this.slice(1) + this[0] // Sposta il primo carattere alla fine della stringa
-}

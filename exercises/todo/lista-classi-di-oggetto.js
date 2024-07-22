02194d385bcdb152f9d2d15157144a842025435c
adellamaggiora <a.dellamaggiora@caen.it>
2024-07-22 17:34:48 +0200

    aggiunto esercizio su come ottenere la lista delle classi appartenenti a un oggetto

---
+++ exercises/todo/lista-classi-di-oggetto.js
@@ -0,0 +1,39 @@
+// scrivere una funzione che prenda in input un oggetto generico e restituisca un array
+// contenete le stringhe con i nome classi che appartengono all'oggetto
+// (duplicati ammessi)
+
+class Vehicle { }
+
+class Car extends Vehicle { }
+
+const lambo = new Car();
+
+/**
+ * versione ricorsiva proposta da @adellamaggiora
+ * qualcuno che proponga un altro modo?
+ */
+function getObjClasses(obj) {
+    if (!obj) {
+        return null;
+    }
+    const className = obj.constructor.name;
+
+    if (className === 'Object') {
+        return [className];
+    }
+
+    return [className, ...getObjClasses(Object.getPrototypeOf(obj))];
+}
+
+console.log(`Lambo class list:`);
+console.log(getObjClasses(lambo));
+
+/**
+ * La classe 'Car' viene inserita 2 volte nell'array perchè
+ * 'lambo' che il suo prototype hanno lo stesso constructor.name
+ */
+
+const prototypeOfLambo = Object.getPrototypeOf(lambo);
+
+console.log(lambo.constructor.name === prototypeOfLambo.constructor.name);
+
